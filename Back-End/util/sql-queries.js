828cdc465f6afcf90f50087ef2e66cafa9fd349b
Micheal Ozdoba <mjozdoba@gmail.com>
2021-03-29 18:03:42 -0700

    Added ES-Lint Rules, mySql, express packages

--- Back-End/util/sql-queries.js
+++ Back-End/util/sql-queries.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql");
 const formatResult = require("./example-util");
 
-const remoteDB = {
-  host: "localhost",
-  user: "michealo_admin",
-  password: "Initial1",
-  database: "michealo_COMP4537Asgn1"
-};
+// const remoteDB = {
+//   host: "localhost",
+//   user: "michealo_admin",
+//   password: "Initial1",
+//   database: "michealo_COMP4537Asgn1",
+// };
 
 const localDB = {
   host: "localhost",
   user: "admin",
   password: "Initial1",
-  database: "COMP4537Asgn1"
+  database: "COMP4537Asgn1",
 };
 
 const db = mysql.createPool({
@@ -20,24 +20,24 @@ const db = mysql.createPool({
   user: localDB.user,
   password: localDB.password,
   database: localDB.database,
-  connectionLimit : 5,
+  connectionLimit: 5,
 });
 
 const deleteRowFromTable = (id) => {
-  const query = `DELETE FROM QUOTES WHERE ID = ${id}`
+  const query = `DELETE FROM QUOTES WHERE ID = ${id}`;
   db.query(query, (err, res) => {
-      if (err) throw err;
-      console.log(res);
+    if (err) throw err;
+    console.log(res);
   });
-}
+};
 
 const insert = ({ quote, author }) => {
   const query = `INSERT INTO QUOTES(QUOTE, AUTHOR) values ('${quote}', '${author}')`;
   db.query(query, (err, res) => {
-      if(err) throw err;
-      console.log(res);
+    if (err) throw err;
+    console.log(res);
   });
-}
+};
 
 const selectAll = ({ res, isAdmin }) => {
   const query = "SELECT * FROM QUOTES";
@@ -52,10 +52,10 @@ const selectAll = ({ res, isAdmin }) => {
       return;
     }
 
-    res.end(exampleUtil(result));
+    res.end(formatResult(result));
     return;
   });
-}
+};
 
 const selectLastRow = ({ res, isGetRequest }) => {
   const query = `SELECT * FROM QUOTES ORDER BY ID DESC LIMIT 1`;
@@ -72,15 +72,15 @@ const selectLastRow = ({ res, isGetRequest }) => {
 
     res.end(formatResult(result));
   });
-}
+};
 
 const update = ({ id, quote, author }) => {
   const query = `UPDATE QUOTES SET QUOTE = "${quote}", AUTHOR = "${author}" WHERE ID = ${id}`;
   db.query(query, (err, res) => {
-      if(err) throw err;
-      console.log(res);
+    if (err) throw err;
+    console.log(res);
   });
-}
+};
 
 module.exports = {
   deleteRowFromTable,
@@ -88,4 +88,4 @@ module.exports = {
   update,
   selectAll,
   selectLastRow,
-}
+};
--- Front-End/js/admin.js
+++ Front-End/js/admin.js
@@ -1,23 +1,23 @@
 // module declarations alphabetized
-const DELETE = 'DELETE';
+const DELETE = "DELETE";
 const endPointRoot = "COMP4537/termproj/api/v1/";
-const GET = 'GET';
-const POST = 'POST';
-const PUT = 'PUT';
+const GET = "GET";
+const POST = "POST";
+const PUT = "PUT";
 const xhttp = new XMLHttpRequest();
 
 // functions alphabetized
 const deleteImage = (id) => {
   xhttp.open(DELETE, endPointRoot, true);
-  xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   const payload = `id=${id}`;
   xhttp.send(payload);
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       // TODO: logic
     }
-  }
-}
+  };
+};
 
 const loadImages = () => {
   xhttp.open(GET, `${endPointRoot}`, true);
@@ -26,8 +26,24 @@ const loadImages = () => {
     if (this.readyState == 4 && this.status == 200) {
       // TODO: logic
     }
-  }
-}
+  };
+};
+
+const createImage = (id) => {
+  let payload = JSON.stringify({
+    id: id,
+  });
+
+  xhttp.open(POST, endPointRoot, true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send(payload);
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // TODO: logic
+    }
+  };
+};
 
 const updateImage = (id) => {
   let payload = JSON.stringify({
@@ -35,12 +51,12 @@ const updateImage = (id) => {
   });
 
   xhttp.open(PUT, endPointRoot, true);
-  xhttp.setRequestHeader('Content-type', 'application/json');
+  xhttp.setRequestHeader("Content-type", "application/json");
   xhttp.send(payload);
 
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       // TODO: logic
     }
-  }
-}
+  };
+};
