3df62a43487e750223207f2f6919a07474c8d066
Micheal Ozdoba <mjozdoba@gmail.com>
2021-04-01 10:07:13 -0700

    Added Status Codes, Resolved CORS issue

--- Back-End/index.js
+++ Back-End/index.js
@@ -2,51 +2,100 @@
 const express = require("express");
 
 // remaining imports alphabetized
-const sqlQueries = require("./util/sql-queries");
-
-const {
-  deleteRowFromTable,
-  insert,
-  selectAll,
-  selectLastRow,
-  update,
-} = sqlQueries;
+const statusCode = require("./http/status-codes");
 
 const app = express();
-const endPointRoot = "4537/termproject/API/V1/";
+const endPointRoot = "/4537/termproject/API/V1";
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("Front-End"));
-app.use("/css", express.static(__dirname + "/css"));
-app.use("/js", express.static(__dirname + "/js"));
+app.use(express.static("Back-End"));
 
 // functions alphabetized
-app.delete(endPointRoot, (req, res) => {
-  // TODO: DELETE Logic
+app.delete(`${endPointRoot}/post/:postid`, (req, res) => {
+  res.writeHead(statusCode.OK, {
+    "Content-Type": "text/html",
+    "Access-Control-Allow-Origin": "*",
+  });
+  const postId = req.query.id;
+  res.status(statusCode.OK).end(`Successfully deleted post with id ${postId}`);
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html")
+app.delete(`${endPointRoot}/location/:location`, (req, res) => {
+  res.writeHead(statusCode.OK, {
+    "Content-Type": "text/html",
+    "Access-Control-Allow-Origin": "*",
+  });
+  const location = req.query.location;
+  res
+    .status(statusCode.OK)
+    .end(`Successfully deleted location with name ${location}`);
 });
 
-app.get("/admin", (req, res) => {
-  res.sendFile(__dirname + "/admin.html")
+app.get(`${endPointRoot}/location/:location`, (req, res) => {
+  res.writeHead(statusCode.OK, {
+    "Content-Type": "text/html",
+    "Access-Control-Allow-Origin": "*",
+  });
+  const location = req.query.location;
+  res
+    .status(statusCode.OK)
+    .end(`Successfully fetched all posts for location ${location}`);
 });
 
-app.get(endPointRoot, (req, res) => {
-    // TODO: GET Logic
+app.get(`${endPointRoot}/location`, (req, res) => {
+  res.writeHead(statusCode.OK, {
+    "Content-Type": "text/html",
+    "Access-Control-Allow-Origin": "*",
+  });
+  res.status(statusCode.OK).end(`Successfully fetched all locations`);
 });
 
-app.post(endPointRoot, (req, res) => {
-    // TODO: POST Logic
+app.post(`${endPointRoot}/login`, (req, res) => {
+  res.writeHead(statusCode.CREATED, {
+    "Content-Type": "text/html",
+    "Access-Control-Allow-Origin": "*",
+  });
+  res.status(statusCode.CREATED).end("Successful Login/Register");
 });
 
-app.put(endPointRoot, (req, res) => {
-    // TODO: PUT Logic
+app.post(`${endPointRoot}/location`, (req, res) => {
+  res.writeHead(statusCode.CREATED, {
+    "Content-Type": "text/html",
+    "Access-Control-Allow-Origin": "*",
+  });
+  res.status(statusCode.CREATED).end("Successful Location Creation");
+});
+
+app.post(`${endPointRoot}/post`, (req, res) => {
+  res.writeHead(statusCode.CREATED, {
+    "Content-Type": "text/html",
+    "Access-Control-Allow-Origin": "*",
+  });
+  const location = req.query.location;
+  res
+    .status(statusCode.CREATED)
+    .end(`Successful Post creation for location ${location}`);
+});
+
+app.put(`${endPointRoot}/user`, (req, res) => {
+  res.writeHead(statusCode.OK, {
+    "Content-Type": "text/html",
+    "Access-Control-Allow-Origin": "*",
+  });
+  res.status(statusCode.OK).end("Successfully updated username");
+});
+
+app.put(`${endPointRoot}/post`, (req, res) => {
+  res.writeHead(statusCode.OK, {
+    "Content-Type": "text/html",
+    "Access-Control-Allow-Origin": "*",
+  });
+  const postid = req.query.postid;
+  res.status(statusCode.OK).end(`Successfully updated post with id ${postid}`);
 });
 
 app.listen(port, () => {
-    console.log("Connected! Waiting for request on port", port);
+  console.log("Connected! Waiting for request on port", port);
 });
