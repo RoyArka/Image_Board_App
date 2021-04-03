c930347cdaaad44e1641dbac8a01b8ce931a3afa
RoyArka <arkaroy0999@gmail.com>
2021-04-03 13:16:37 -0700

    added basic documentation for GET stats and location

--- Front-End/js/admin.js
+++ Front-End/js/admin.js
@@ -6,23 +6,15 @@ const xhttp = new XMLHttpRequest();
 
 //AJAX Stats GET
 const statsGet = () => {
-  const testData =
-    '[{"ID":2,"Method":"GET","Endpoint":"/4537/termproject/API/V1/location","Requests":39},{"ID":3,"Method":"GET","Endpoint":"/4537/termproject/API/V1/stats","Requests":11},{"ID":4,"Method":"POST","Endpoint":"/4537/termproject/API/V1/location","Requests":46}]';
-  // xhttp.open(GET, `${endPointRoot}/stats`, true);
-  // xhttp.send();
-  // xhttp.onreadystatechange = function () {
-  //   if (this.readyState == 4 && this.status == 200) {
-  //     const rootStats = document.getElementById("rootStats");
-  //     const statsResponse = JSON.parse(this.response);
-  //     const statsResponseTest = JSON.parse(testData);
-
-  //     rootStats.innerHTML = formatStatisticsResult(statsResponseTest);
-  //   }
-  // };
-  const rootStats = document.getElementById("rootStats");
-  // const statsResponse = JSON.parse(this.response);
-  const statsResponseTest = JSON.parse(testData);
-  rootStats.innerHTML = formatStatisticsResult(statsResponseTest);
+  xhttp.open(GET, `${endPointRoot}/stats`, true);
+  xhttp.send();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const rootStats = document.getElementById("rootStats");
+      const statsResponse = JSON.parse(this.response);
+      rootStats.innerHTML = formatStatisticsResult(statsResponse);
+    }
+  };
 };
 
 const formatStatisticsResult = (result) => {
