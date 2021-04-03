32371dbc389bb26ffe35652337c74d9beadeb799
RoyArka <arkaroy0999@gmail.com>
2021-04-02 17:05:10 -0700

    finished static front end for home page

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,8 +1,7 @@
 //express server for front-end routing
 
-// library imports first
+//library imports first
 const express = require("express");
-
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -12,14 +11,31 @@ app.use(express.static("Front-End"));
 app.use("/css", express.static(__dirname + "/css"));
 app.use("/js", express.static(__dirname + "/js"));
 
+//home page
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
+  res.sendFile(__dirname + "/index.html");
 });
-  
-app.get("/admin", (req, res) => {
-    res.sendFile(__dirname + "/admin.html")
+
+//profile page
+app.get("/profle", (req, res) => {
+  res.sendFile(__dirname + "/profile.html");
+});
+
+//stats page for requests
+app.get("/stats", (req, res) => {
+  res.sendFile(__dirname + "/admin.html");
+});
+
+//documentation page
+app.get("/doc", (req, res) => {
+  res.sendFile(__dirname + "/documentation.html");
+});
+
+//logsout to login page
+app.get("/logout", (req, res) => {
+  res.sendFile(__dirname + "/login.html");
 });
 
 app.listen(port, () => {
-    console.log("Connected! Waiting for request on port", port);
+  console.log("Connected! Waiting for request on port", port);
 });
